2c8cec321d0ee4d4df8357f8e6ec6f46645ae9ea
Reshy <62392694+thReshard@users.noreply.github.com>
2020-03-20 14:32:42 +0100

    Szkielet
    Zaprogramowałem na ten moment chodzenie postaci w kierunkach lewo/prawo, prostą grawitację, skok i kolizje.

---
+++ myGame/myGame/myGame/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace myGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Timer timer1;
+            this.screen = new System.Windows.Forms.Panel();
+            this.character = new System.Windows.Forms.PictureBox();
+            this.block = new System.Windows.Forms.PictureBox();
+            timer1 = new System.Windows.Forms.Timer(this.components);
+            this.screen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.character)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.block)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // screen
+            // 
+            this.screen.Controls.Add(this.block);
+            this.screen.Controls.Add(this.character);
+            this.screen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.screen.Location = new System.Drawing.Point(0, 0);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(800, 450);
+            this.screen.TabIndex = 0;
+            // 
+            // character
+            // 
+            this.character.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.character.Location = new System.Drawing.Point(1, 425);
+            this.character.Name = "character";
+            this.character.Size = new System.Drawing.Size(25, 25);
+            this.character.TabIndex = 0;
+            this.character.TabStop = false;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 1;
+            timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // block
+            // 
+            this.block.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.block.Location = new System.Drawing.Point(382, 400);
+            this.block.Name = "block";
+            this.block.Size = new System.Drawing.Size(100, 50);
+            this.block.TabIndex = 1;
+            this.block.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.screen);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.screen.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.character)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.block)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel screen;
+        private System.Windows.Forms.PictureBox character;
+        private System.Windows.Forms.PictureBox block;
+    }
+}
+
---
+++ myGame/myGame/myGame/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myGame
+{
+    public partial class Form1 : Form
+    {
+
+        bool left;
+        bool right;
+
+        bool jump;
+        int G = 20; //wysokosc skoku
+        int force;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //kolizja boczna
+
+            if (character.Right > block.Left && character.Left < block.Right - character.Width / 2 && character.Bottom > block.Top)
+            {
+                right = false;
+            }
+            if (character.Left < block.Right && character.Right > block.Left + character.Width / 2 && character.Bottom > block.Top)
+            {
+                left = false;
+            }
+
+
+            if (right == true) { character.Left += 5; }
+            if (left == true) { character.Left -= 5; }
+
+            if( jump==true )
+            {
+                //spadanie jezeli gracz skoczyl
+                character.Top -= force;
+                force -= 1;
+            }
+
+            if(character.Top + character.Height >= screen.Height)
+            {
+                character.Top = screen.Height - character.Height;
+                jump = false;
+            }
+            else
+            {
+                character.Top += 5;
+            }
+
+
+            //Kolizja od gory
+
+            if(character.Left + character.Width - 1 > block.Left && character.Left + character.Width + 5 < block.Left + block.Width + character.Width && character.Top + character.Height >= block.Top && character.Top < block.Top)
+            {
+                character.Top = screen.Height - block.Height - character.Height;
+                force = 0;
+                jump = false;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Right) { right = true; }
+            if(e.KeyCode == Keys.Left) { left = true; }
+            if(e.KeyCode == Keys.Escape) { this.Close(); } //zamykanie poprzez esc.
+
+            if(jump!=true)
+            {
+                if(e.KeyCode == Keys.Space)
+                {
+                    jump = true;
+                    force = G;
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Right) { right = false; }
+            if(e.KeyCode == Keys.Left) { left = false; }
+        }
+    }
+}
