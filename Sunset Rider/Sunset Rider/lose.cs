c6e17a5f78a98431aa7f98c75e3647c19c3402c0
Oliwia <62391424+olimari@users.noreply.github.com>
2020-05-29 22:10:55 +0200

    29/05 zmiany

--- Sunset Rider/Sunset Rider/lose.cs
+++ Sunset Rider/Sunset Rider/lose.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WMPLib;
 
 namespace Sunset_Rider
 {
@@ -15,10 +16,12 @@ namespace Sunset_Rider
         public lose()
         {
             InitializeComponent();
+            player.URL = "fail.wav";
         }
 
         private void menu_Click(object sender, EventArgs e)
         {
+            player.controls.play();
             Form1.pause = false;
             pauza.formclose = true;
             this.Close();
@@ -26,6 +29,7 @@ namespace Sunset_Rider
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
+            player.controls.play();
             Form1.pause = false;
             pauza.formrestart = true;
             this.Close();
